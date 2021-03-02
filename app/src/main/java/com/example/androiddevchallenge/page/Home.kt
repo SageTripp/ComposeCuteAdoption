28739e081fd058ed4402aadee39ab187cd2791a6
SageTripp <zhangst0720@163.com>
2021-03-02 12:42:54 +0800

    deal Wildcard import

--- app/src/main/java/com/example/androiddevchallenge/page/Home.kt
+++ app/src/main/java/com/example/androiddevchallenge/page/Home.kt
@@ -2,11 +2,7 @@ package com.example.androiddevchallenge.page
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
